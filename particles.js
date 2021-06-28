1cabe2c841a2af870f9421484ac625a930b84a78
sidhuPaji-777 <82391641+sidhuPaji-777@users.noreply.github.com>
2021-06-28 16:04:59 +0530

    Update particles.js

--- particles.js
+++ particles.js
@@ -8,7 +8,7 @@ class Particles
             "density" : 1.0   
         }
 
-        this.r = 12;
+        this.r = 7;
         this.body=Bodies.circle(x,y,this.r,option);
         World.add(world,this.body);
 
@@ -17,24 +17,19 @@ class Particles
 
         // this.image = loadImage("images/stone.png");
     }
-    
-    display()
-    {
-        var pos=this.body.position;
-        var angle = this.body.angle;
 
-        push();
-        // translate(pos.x, pos.y);
-        rotate(angle);
-
-        fill(this.color);
-        // fill(rgb(255, 2, 5));
-        ellipseMode(CENTER);
-
-        ellipse(pos.x+2, pos.y+2, this.r,this.r);  
-        // imageMode(CENTER);
-        // image(this.image, pos.x, pos.y, this.r*2, this.r*2);
-        pop();
-    }
+display()
+{
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push(); translate(pos.x, pos.y);
+    rotate(angle);
+    noStroke();
+    fill(this.color);
+    ellipseMode(RADIUS);
+    ellipse(0, 0, this.r,this.r);
+    pop();
+}
 
 }
+
